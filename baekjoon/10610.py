9cc8c868c9c65854dd06a0b804beeab022951d7b
unpieceof <101758997+unpieceof@users.noreply.github.com>
2022-04-05 09:00:24 +0900

    10610. 30

---
+++ baekjoon/10610.py
@@ -0,0 +1,19 @@
+# 10610. 30
+# sort, reverse의 Type은 None임 -> list return이 아니라 just operate?
+# 가지치기 가능한 부분 확인 필요
+num = input()
+list_num = list(num)
+sum_num = 0
+z_flag = False
+
+for i in list_num:
+    sum_num += int(i)
+    if i == '0':
+        z_flag = True
+
+if sum_num%3 == 0 and z_flag == True:
+    list_num.sort()
+    list_num.reverse()
+    print(''.join(list_num))
+else:
+    print(-1)
