386cdfc7b9f771143ceeb6033d51a129bd85ba87
unpieceof <101758997+unpieceof@users.noreply.github.com>
2022-04-21 08:06:56 +0900

    Create 10814.py

---
+++ baekjoon/10814.py
@@ -0,0 +1,9 @@
+# 10814. 나이순 정렬
+num = int(input())
+members = []
+for _ in range(num):
+    members.append(list(input().split()))
+
+members.sort(key=lambda x:int(x[0]))
+for member in members:
+    print(member[0], member[1])
