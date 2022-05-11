94bb3d64391b2ba60141d4657ee05f9ac0e57c5d
unpieceof <unpieceof@gmail.com>
2022-05-11 20:59:25 +0900

    217. Contains Duplicate

---
+++ leetcode/217.py
@@ -0,0 +1,11 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        chk_dict = dict()
+        for num in nums:
+            try:
+                if chk_dict[num] == 1:
+                    return True
+            except:
+                chk_dict[num] = 1
+        
+        return False
